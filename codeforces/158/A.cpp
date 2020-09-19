f4fd0ce42b8295aadd4efcf84c1a1155f43ace00
Sughandhan S <sughandhan@yahoo.com>
2020-09-19 11:28:00 +0530

    Add solution for problem `A - Next Round` Link: https://codeforces.com/contest/158/problem/A Tags: *special problem, implementation, *800 Ref: https://codeforces.com/contest/158/submission/93166583

---
+++ codeforces/158/A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int num=arr[k-1], count=0;
+    for(int j=0;j<n;j++){
+        if(arr[j]>=num && arr[j]>0){
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
