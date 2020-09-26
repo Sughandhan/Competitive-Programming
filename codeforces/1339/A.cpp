5897fbe35712ad1b204682d9e65ca2612a993d26
Sughandhan S <sughandhan@yahoo.com>
2020-09-26 14:45:00 +0530

    Add solution for problem `A - Filling Diamonds` Link: https://codeforces.com/contest/1339/problem/A Tags: brute force, dp, implementation, math, *900 Ref: https://codeforces.com/contest/1339/submission/93857625

---
+++ codeforces/1339/A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        // int x=1;
+        // while(n>0){
+        //     x*=n;
+        //     n--;
+        // }
+        cout<<n<<endl;
+    }
+    return 0;
+}
