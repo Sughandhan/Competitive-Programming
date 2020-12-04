2440782bfb5e942e9d22a8e7cb14183d3e594ecd
Sughandhan S <sughandhan@yahoo.com>
2020-12-04 21:29:00 +0530

    Add solution for problem `A - Lineland Mail` Link: https://codeforces.com/contest/567/problem/A Tags: greedy, implementation, *900 Ref: https://codeforces.com/contest/567/submission/100388418

--- codeforces/567/A.cpp
+++ codeforces/567/A.cpp
@@ -1,31 +1,30 @@
-#include<iostream>
-#include<algorithm>
- 
+#include <iostream>
+#include <algorithm>
 using namespace std;
- 
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    for(int i=0;i<n;i++){
-        long long int maxi;
-        long long int mini;
-        if(i==0){
-            maxi = arr[n-1]- arr[0];
-            mini = arr[1]- arr[0];
-        }
-        else if(i==n-1){
-            maxi = arr[n-1]- arr[0];
-            mini = arr[n-1]- arr[n-2];
-        }
-        else{
-            mini= min(arr[i+1]-arr[i],arr[i]-arr[i-1]);
-            maxi= max(arr[i]-arr[0],arr[n-1]-arr[i]);
-        }
-        cout<<mini<<" "<<maxi<<"\n";
-    }
-    return 0;
+
+int main() {
+	 int n;
+	 cin>>n;
+	 int arr[n];
+	 for(int i=0;i<n;i++){
+		 cin>>arr[i];
+	 }
+	 for(int i=0;i<n;i++){
+		 long long  mini;
+		 long long maxi;
+		 if(!i){
+			mini=abs(arr[i]-arr[i+1]);
+			maxi=abs(arr[i]-arr[n-1]);
+		 }
+		 else if(i==n-1){
+			 maxi=abs(arr[i]-arr[0]);
+			 mini=abs(arr[i]-arr[i-1]);
+		 }
+		 else{
+			 mini=min(abs(arr[i]-arr[i+1]),abs(arr[i]-arr[i-1]));
+			 maxi=max(abs(arr[i]-arr[0]),abs(arr[i]-arr[n-1]));
+		 }
+		 cout<<mini<<" "<<maxi<<"\n";
+	 }
+	return 0;
 }
