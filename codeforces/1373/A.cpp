ce1d69e0d7b1a0b605b213bc10eae97b14bb85d7
Sughandhan S <sughandhan@yahoo.com>
2021-01-12 12:59:00 +0530

    Add solution for problem `A - Donut Shops` Link: https://codeforces.com/contest/1373/problem/A Tags: greedy, implementation, math, *1000 Ref: https://codeforces.com/contest/1373/submission/104090666

--- codeforces/1373/A.cpp
+++ codeforces/1373/A.cpp
@@ -9,17 +9,23 @@ int main(){
         long long int a, b, c;
         cin>>a>>b>>c;
         long long int c1, c2;
-        if(a<c){
+        if(a>c){
+            c1=-1;
+        }
+        else if(a<c){
             c1=1;
+        }
+        else if(a*(b+1)<=2*c){
+            c1=b+1;
         }
         else{
             c1=-1;
         }
-        if(a*b>c){
-            c2=b;
+        if(a*b<=c){
+            c2=-1;
         }
         else{
-            c2=-1;
+            c2=b;
         }
         cout<<c1<<" "<<c2<<endl;
     }
