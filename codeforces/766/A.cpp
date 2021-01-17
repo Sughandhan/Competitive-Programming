a2a1ef2db338bd4b1b49dc56f88c43061a10382c
Sughandhan S <sughandhan@yahoo.com>
2021-01-18 02:54:00 +0530

    Add solution for problem `A - Mahmoud and Longest Uncommon Subsequence` Link: https://codeforces.com/contest/766/problem/A Tags: constructive algorithms, strings, *1000 Ref: https://codeforces.com/contest/766/submission/104619342

---
+++ codeforces/766/A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    string a, b;
+    cin>>a>>b;
+    if(a==b){
+        cout<<-1;
+    }
+    else{
+        cout<<max(a.size(), b.size());
+    }
+    return 0;
+}
