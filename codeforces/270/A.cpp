00cbf2e76193954f06b854416d55da7cd4820fa0
Sughandhan S <sughandhan@yahoo.com>
2021-02-01 14:33:00 +0530

    Add solution for problem `A - Fancy Fence` Link: https://codeforces.com/contest/270/problem/A Tags: geometry, implementation, math, *1100 Ref: https://codeforces.com/contest/270/submission/106172275

---
+++ codeforces/270/A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int a;
+        cin>>a;
+        if( 360%(180-a)==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
