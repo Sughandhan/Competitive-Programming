6faed886272021af4f160d0cdba7fd4801bcce78
Sughandhan S <sughandhan@yahoo.com>
2021-02-05 01:21:00 +0530

    Add solution for problem `A - Laptops` Link: https://codeforces.com/contest/456/problem/A Tags: sortings, *1100 Ref: https://codeforces.com/contest/456/submission/106488931

--- codeforces/456/A.cpp
+++ codeforces/456/A.cpp
@@ -1,20 +1,19 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+
 using namespace std;
-int main()
-{
-    int n,i;
-    scanf("%d",&n);
-    pair<int,int>a[n];
-    for(i=0;i<n;i++){
-        scanf("%d%d",&a[i].first,&a[i].second);
-    }
-    sort(a,a+n);
-    for(i=0;i<n-1;i++){
-        if(a[i].first<a[i+1].first&&a[i].second>a[i+1].second){
-            printf("Happy Alex\n");
+
+int main(){
+    int n;
+    cin>>n;
+    int a, b, ok=0;
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        if(a!=b){
+            cout<<"Happy Alex";
             return 0;
         }
     }
-    printf("Poor Alex\n");
+    cout<<"Poor Alex";
     return 0;
 }
