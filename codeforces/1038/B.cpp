17085d38e3d98e395da26e1c1ff348c94a2ed4de
Sughandhan S <sughandhan@yahoo.com>
2021-03-11 13:32:00 +0530

    Add solution for problem `B - Non-Coprime Partition` Link: https://codeforces.com/contest/1038/problem/B Tags: constructive algorithms, math, *1100 Ref: https://codeforces.com/contest/1038/submission/109684550

--- codeforces/1038/B.cpp
+++ codeforces/1038/B.cpp
@@ -7,6 +7,11 @@ int main(){
     int n;
     cin>>n;
     if(n<=2) cout<<"No";
+    else if(n==3){
+        cout<<"Yes"<<endl;
+        cout<<1<<" "<<2<<endl;
+        cout<<2<<" "<<1<<" "<<3;
+    }
     else{
         cout<<"Yes"<<endl;
         int x = (n%2==0)? (n/2): ((n+1)/2);
