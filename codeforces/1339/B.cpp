771e7aa72eea4cd27f199e45f38a986e2ed2991f
Sughandhan S <sughandhan@yahoo.com>
2021-04-27 13:01:00 +0530

    Add solution for problem `B - Sorted Adjacent Differences` Link: https://codeforces.com/contest/1339/problem/B Tags: constructive algorithms, sortings, *1200 Ref: https://codeforces.com/contest/1339/submission/114338463

--- codeforces/1339/B.cpp
+++ codeforces/1339/B.cpp
@@ -1,8 +1,12 @@
 #include<bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 int main() {
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
 	int t;
 	cin >> t;
 	while (t--) {
@@ -15,15 +19,15 @@ int main() {
 			vi.push_back(x);
 		}
 		sort(vi.begin(), vi.end());
- 
+
 		for (long long int i = n / 2; i < n; i++) {
 			if (n - 1 - i == i) {
 				vf.push_back(vi[i]);
 				//break;
 			}
 			else {
-				vf.push_back(vi[i]);
 				vf.push_back(vi[n - i - 1]);
+				vf.push_back(vi[i]);
 			}
 		}
 		for (long long int i = 0; i < n; i++) {
