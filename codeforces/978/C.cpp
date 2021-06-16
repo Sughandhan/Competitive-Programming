7825c0bc20d972716ad35e2647b6410124600af6
Sughandhan S <sughandhan@yahoo.com>
2021-06-16 22:12:00 +0530

    Add solution for problem `C - Letters` Link: https://codeforces.com/contest/978/problem/C Tags: binary search, implementation, two pointers, *1000 Ref: https://codeforces.com/contest/978/submission/119653961

--- codeforces/978/C.cpp
+++ codeforces/978/C.cpp
@@ -1,44 +1,32 @@
-// BATMAN WAS HERE
- 
-#include<bits\stdc++.h>
- 
+
+#include<bits/stdc++.h>
+#include<stdio.h>
 using namespace std;
- 
- 
-#define inf 1000000007
-#define pb push_back
-#define mp make_pair
-#define fr(i,a,b) for(int i=a;i<=b;++i)
-#define frn(i,a,b) for(int i=a;i>=b;--i)
-#define all(v) (v).begin(), (v).end()
-#define tr(v, it) for(vector<long long int> ::iterator (it) = (v).begin(); it!= (v).end() ; it++)
-#define deb(x) cout<< #x <<" "<< x
-#define ll long long int
-#define speedup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
- 
- 
- 
-int main() {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    speedup;
-    __int64 n, m;
-    cin >> n >> m;
-    vector<__int64> v(n);
-    for (__int64 i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    __int64 sum = 0, j = 0;
-    for (__int64 i = 0; i < m; i++) {
-        __int64 p;
-        cin >> p;
-        while (sum + v[j] < p) {
-            sum += v[j];
-            j++;
-        }
-        cout << j + 1 << " " << p - sum << '\n';
-    }
-    return 0;
+
+
+#define ll                  long long
+#define scl(n)              scanf("%lld", &n)
+#define fr(i,n)             for (ll i=0;i<n;i++)
+#define fr1(i,n)            for(ll i=1;i<=n;i++)
+
+int main()
+{
+
+    ll m,n,t=1,c,d,i,j,k,x,y,z,l,q,r;
+
+      ll cnt=0,sum=0;
+      scl(n);scl(m);
+      ll a[n], b[m];
+
+       fr1(i,n)cin>>a[i];
+
+       fr(i, m)
+       {
+           cin>>x;
+           while(sum+a[t]< x)sum+=a[t++];
+         //cout<<sum<<endl;
+           printf("%lld %lld\n", t, x-sum);
+       }
+
+return 0;
 }
