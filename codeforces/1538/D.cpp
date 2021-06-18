2e0f19db885d3d60aada5d1ea6074dc99e968b8d
Sughandhan S <sughandhan@yahoo.com>
2021-06-18 14:04:00 +0530

    Add solution for problem `D - Another Problem About Dividing Numbers` Link: https://codeforces.com/contest/1538/problem/D Tags: constructive algorithms, math, number theory Ref: https://codeforces.com/contest/1538/submission/119772729

--- codeforces/1538/D.cpp
+++ codeforces/1538/D.cpp
@@ -1,75 +1,56 @@
-// BATMAN WAS HERE
-
-#include<bits\stdc++.h>
-
+#include <bits/stdc++.h> 
 using namespace std;
 
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-#define inf 1000000007
-#define pb push_back
-#define mp make_pair
-#define fr(i,a,b) for(int i=a;i<=b;++i)
-#define frn(i,a,b) for(int i=a;i>=b;--i)
-#define all(v) (v).begin(), (v).end()
-#define tr(v, it) for(vector<long long int> ::iterator (it) = (v).begin(); it!= (v).end() ; it++)
-#define deb(x) cout<< #x <<" "<< x
-#define ll long long int
-#define speedup ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-
-
-
-int fac(int n) {
-	int c = 0;
-	while (n % 2 == 0) {
-		c++;
-		n /= 2;
-	}
-	for (int i = 3; i * i <= n + 1; i += 2) {
-		while (n % i == 0) {
-			n = n / i;
-			c++;
-		}
-	}
-	if (n > 2) {
-		c++;
-	}
-	return c;
+int prime_factors(int n){
+    int count = 0;
+    while(n % 2 == 0){
+        n /= 2;
+        count++;
+    }
+    for(int i = 3;i*i <= n + 1;i += 2){
+        while(n % i == 0){
+            count++;
+            n /= i;
+        }
+    }
+    if(n > 2){
+        count++;
+    }
+    return count;
 }
-
-
-int main() {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-	speedup;
-	int t;
-	cin >> t;
-	while (t--) {
-		int a, b, k;
-		cin >> a >> b >> k;
-		if (k == 1 && a == b) {
-			cout << "NO" << '\n';
-			continue;
-		}
-		int n, m;
-		if (a == b) {
-			m = 0;
-		}
-		else if (__gcd(a, b) == min(a, b)) {
-			m = 1;
-		}
-		else {
-			m = 2;
-		}
-		n = fac(a) + fac(b);
-		if (m <= k && k <= n) {
-			cout << "YES" << '\n';
-		}
-		else {
-			cout << "NO" << '\n';
-		}
-
-	}
-	return 0;
+ 
+int main()
+{  
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,k;
+        cin >> a >> b >> k;
+        int lower = 0;
+        if(a == b && k == 1){
+            cout << "NO" << "\n";
+            continue;
+        }
+        if(a == b){
+            lower = 0;
+        }else if(__gcd(a,b) == min(a,b)){
+            lower = 1;
+        }else{
+            lower = 2;
+        }
+        int max_moves = prime_factors(a) + prime_factors(b);
+        if(lower <= k && k <= max_moves){
+            cout << "YES" << "\n";
+        }else{
+            cout << "NO" << "\n";
+        }
+    }
+    return 0;
 }
