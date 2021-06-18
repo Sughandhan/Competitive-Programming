f3e7add0520b2431ff603b9147c75eae34bbc0c1
Sughandhan S <sughandhan@yahoo.com>
2021-06-18 15:41:00 +0530

    Add solution for problem `B - Chloe and the sequence ` Link: https://codeforces.com/contest/743/problem/B Tags: binary search, bitmasks, constructive algorithms, implementation, *1200 Ref: https://codeforces.com/contest/743/submission/119781141

---
+++ codeforces/743/B.cpp
@@ -0,0 +1,40 @@
+// BATMAN WAS HERE
+
+#include<bits\stdc++.h>
+
+using namespace std;
+
+
+#define inf 1000000007
+#define pb push_back
+#define mp make_pair
+#define fr(i,a,b) for(int i=a;i<=b;++i)
+#define frn(i,a,b) for(int i=a;i>=b;--i)
+#define all(v) (v).begin(), (v).end()
+#define tr(v, it) for(vector<long long int> ::iterator (it) = (v).begin(); it!= (v).end() ; it++)
+#define deb(x) cout<< #x <<" "<< x
+#define ll long long int
+#define speedup ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	speedup;
+	ll n, k;
+	cin >> n >> k;
+	if (k % 2 == 1) {
+		cout << 1;
+	}
+	else {
+		ll ans = 1;
+		while (k % 2 == 0) {
+			ans++;
+			k /= 2;
+		}
+		cout << ans;
+	}
+	return 0;
+}
