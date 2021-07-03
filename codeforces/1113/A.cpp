90277fa3d4017029357680432c756d8e9ba38561
Sughandhan S <sughandhan@yahoo.com>
2021-07-03 16:24:00 +0530

    Add solution for problem `A - Sasha and His Trip` Link: https://codeforces.com/contest/1113/problem/A Tags: dp, greedy, math, *900 Ref: https://codeforces.com/contest/1113/submission/121179827

---
+++ codeforces/1113/A.cpp
@@ -0,0 +1,42 @@
+// BATMAN WAS HERE
+
+#include<bits\stdc++.h>
+
+using namespace std;
+
+
+#define inf 1000000007
+#define pb push_back
+#define fr(i,a,b) for(int i=a;i<b;++i)
+#define frn(i,a,b) for(int i=a;i>=b;--i)
+#define all(v) (v).begin(), (v).end()
+#define tr(v, it) for(vector<long long int> ::iterator (it) = (v).begin(); it!= (v).end() ; it++)
+#define deb(x) cout<< #x <<" "<< x
+#define ll long long int
+#define speedup ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define N 100005
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	speedup;
+	ll n, v;
+	cin >> n >> v;
+	if (v >= n) {
+		cout << n - 1;
+	}
+	else {
+		ll sum = 0;
+		sum += v;
+		ll add = 2;
+		ll upto = n - v - 1;
+		for (ll i = 0 ; i < upto; i++) {
+			sum += add;
+			add++;
+		}
+		cout << sum;
+	}
+}
