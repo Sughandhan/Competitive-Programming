6cd3032879d47a1401bb7fa03cfb398e8870b9bf
Sughandhan S <sughandhan@yahoo.com>
2021-07-05 23:53:00 +0530

    Add solution for problem `B - CopyCopyCopyCopyCopy` Link: https://codeforces.com/contest/1325/problem/B Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1325/submission/121420963

---
+++ codeforces/1325/B.cpp
@@ -0,0 +1,39 @@
+// BATMAN WAS HERE
+
+#include<bits\stdc++.h>
+
+using namespace std;
+
+
+#define inf 1000000007
+#define pb push_back
+#define fr(i,a,b) for(int i=a;i<b;++i)
+#define frn(i,a,b) for(int i=a;i>=b;--i)
+#define all(v) (v).begin(), (v).end()
+#define tr(v, it) for(vector<long long int> ::iterator (it) = (v).begin(); it!= (v).end() ; it++)
+#define deb(x) cout<< #x <<" "<< x
+#define ll long long int
+#define speedup ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define N 100005
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	speedup;
+	ll t = 1;
+	cin >> t;
+	while (t--) {
+		ll n;
+		cin >> n;
+		set<ll> s;
+		for (ll i = 0; i < n; i++) {
+			ll x;
+			cin >> x;
+			s.insert(x);
+		}
+		cout << s.size() << "\n";
+	}
+}
