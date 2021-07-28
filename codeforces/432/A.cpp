d4d9d32b90e7bd9b228c5a53acf8ae7023ff19cc
Sughandhan S <sughandhan@yahoo.com>
2021-07-28 22:47:00 +0530

    Add solution for problem `A - Choosing Teams` Link: https://codeforces.com/contest/432/problem/A Tags: greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/432/submission/124054418

---
+++ codeforces/432/A.cpp
@@ -0,0 +1,41 @@
+// BATMAN WAS HERE
+
+#include<bits\stdc++.h>
+
+using namespace std;
+
+
+#define inf 1000000007
+#define pb push_back
+#define fr(i,a,b) for(int i=a;i<b;++i)
+#define frn(i,a,b) for(int i=a;i>=b;--i)
+#define all(v) (v).begin(), (v).end()
+#define tr(v, it) for(vector<long long int> ::iterator (it) = (v).begin(); it!= (v).end() ; it++)
+#define deb(x) cout<< #x <<" "<< x
+#define ll long long int
+#define speedup ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define N 100005
+
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	speedup;
+	// cout << 69;
+	ll n, k;
+	cin >> n >> k;
+	ll c = 0;
+	for (ll i = 0; i < n; i++) {
+		ll x;
+		cin >> x;
+		if (x + k <= 5) {
+			c++;
+		}
+	}
+	cout << c / 3;
+	return 0;
+}
+
